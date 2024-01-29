1d1eff0413f73026ce8ca0b9199b241eab95c8e7
Anas En-nadir <anasenadir2001@gmail.com>
2024-01-29 16:39:00 +0100

    until redis

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,26 @@
-FROM node:latest
+FROM node:latest as base
 
+# ==============================================
+
+From base as production
 WORKDIR /app
+COPY package.json .
+# Install the dependencies
+RUN npm install --only=production
+COPY . .
+EXPOSE 4000
+CMD [ "npm", "start" ]
 
+# ==============================================
+
+From base as development
+WORKDIR /app
 COPY package.json .
 # Install the dependencies
 RUN npm install
-
 RUN npm install -g nodemon
 # ADD /src /app/src
-
 COPY . .
-
 EXPOSE 4000
-
 # Start the server
-# CMD ["nodemon", "--watch", ".", "--legacy-watch", "src/index.js"]
 CMD [ "npm", "run", "start-dev" ]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require("redis")
+
 const app = express();
 const PORT = 4000;
 
@@ -14,7 +16,48 @@ mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to DB successfully"))
     .catch((err) => console.error("Failed to connect to DB", err));
 
-app.get("/", (req, res) => {
+
+// connecting to redis
+const REDIS_HOST = "redis";
+const REDIS_PORT = 6379;
+let redisClient
+(async() => {
+    redisClient  = redis.createClient({
+        url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+    })
+    redisClient.on("error", (err) => console.log(`Error: ${err}`))
+    redisClient.on("connect", ()=> console.log("Connect to redis..."))
+    await redisClient.connect()
+})()
+
+
+    
+
+app.get("/", async (req, res) => {
+    let isCashed = false;
+    let result;
+
+    try{
+        result =  await redisClient.get("title")
+        if (result)
+        {
+            isCashed = true
+        }
+        else
+        {
+            result = "hello from redis"
+            await redisClient.set("title", result)
+        }
+        
+        res.send({
+            isCashed: true,
+            data: result
+        })
+    }catch (error)
+    {
+        console.error(error);
+        res.status(404).send("Data unavailable");
+    }
     res.send("<h1>Hello from Node</h1>");
 });
 
